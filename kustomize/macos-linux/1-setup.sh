e6c13e00ef683840234c0d9004ed4e03df96968b
Dávid Csendes <csendes.david@alerant.hu>
2023-12-03 16:05:15 +0100

    Fix incorrect namespace creation

--- kustomize/macos-linux/1-setup.sh
+++ kustomize/macos-linux/1-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-kubectl apply -k ../resources/test-namespace.yaml
-kubectl apply -k ../resources/prod-namespace.yaml
+kubectl apply -f ../resources/test-namespace.yaml
+kubectl apply -f ../resources/prod-namespace.yaml
 kubectl apply -k ../resources/overlays/test
 kubectl apply -k ../resources/overlays/prod
--- kustomize/windows/1-setup.ps1
+++ kustomize/windows/1-setup.ps1
@@ -1,4 +1,4 @@
-kubectl apply -k ..\resources\test-namespace.yaml
-kubectl apply -k ..\resources\prod-namespace.yaml
+kubectl apply -f ..\resources\test-namespace.yaml
+kubectl apply -f ..\resources\prod-namespace.yaml
 kubectl apply -k ..\resources\overlays\test
 kubectl apply -k ..\resources\overlays\prod
