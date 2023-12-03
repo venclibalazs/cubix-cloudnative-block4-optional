b9e58c6215d9f7934599b34f8fa2a0ae6f6cd808
Dávid Csendes <csendes.david@alerant.hu>
2023-12-03 14:44:30 +0100

    Instead of grafana hostname, use other hostname

--- kustomize/macos-linux/2-test.sh
+++ kustomize/macos-linux/2-test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl http://grafana.cubix.localhost:8080/frontend?message=Hello
-curl http://grafana.cubix.localhost:8080/frontend/local?message=Hello
+curl http://other.cubix.localhost:8080/frontend?message=Hello
+curl http://other.cubix.localhost:8080/frontend/local?message=Hello
 curl http://application.cubix.localhost:8080/frontend?message=Hello
 curl http://application.cubix.localhost:8080/frontend/local?message=Hello
--- kustomize/windows/2-test.ps1
+++ kustomize/windows/2-test.ps1
@@ -1,4 +1,4 @@
-curl http://grafana.cubix.localhost:8080/frontend?message=Hello
-curl http://grafana.cubix.localhost:8080/frontend/local?message=Hello
+curl http://other.cubix.localhost:8080/frontend?message=Hello
+curl http://other.cubix.localhost:8080/frontend/local?message=Hello
 curl http://application.cubix.localhost:8080/frontend?message=Hello
 curl http://application.cubix.localhost:8080/frontend/local?message=Hello
