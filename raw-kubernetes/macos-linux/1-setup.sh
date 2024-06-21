c901870759e9a1726a0d865a388dad95c615adf3
73691 <balazs.vencli@4ig.hu>
2024-06-21 14:47:38 +0200

    fix raw-kubernetes

--- raw-kubernetes/macos-linux/1-setup.sh
+++ raw-kubernetes/macos-linux/1-setup.sh
@@ -4,5 +4,6 @@ kubectl apply -f ../resources/frontend-namespace.yaml
 kubectl apply -f ../resources/backend-namespace.yaml
 kubectl apply -f ../resources/frontend-deployment.yaml
 kubectl apply -f ../resources/frontend-service.yaml
+kubectl apply -f ../resources/frontend-ingress.yaml
 kubectl apply -f ../resources/backend-deployment.yaml
 kubectl apply -f ../resources/backend-service.yaml
