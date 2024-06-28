6cb0a60e4a146acd20d8c4b6b5b484e2b829c450
73691 <balazs.vencli@4ig.hu>
2024-06-28 14:09:29 +0200

    re-usable helm

--- helm/macos-linux/1-setup.sh
+++ helm/macos-linux/1-setup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 kubectl apply -f ../resources/namespace.yaml
-helm upgrade frontend ../resources/spring --install -n op4-helm
+helm upgrade frontend ../resources/common -f ../resources/spring/values.yaml --install -n op4-helm
